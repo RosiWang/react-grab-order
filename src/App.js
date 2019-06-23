262358d582885534f91f2d887de41ca03319af1a
RosiWang <rosiWang@163.com>
2019-06-23 19:43:42 +0800

    add post test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import displayImg from './img/test.JPG'
+import {Button} from 'antd'
 
 class App extends React.Component{
 
@@ -11,17 +12,40 @@ class App extends React.Component{
 
   componentDidMount(){
     console.log('start...');
-    fetch('http://localhost:7002/getdata',{
+    fetch('http://localhost:7002/httpinit',{
       method:'GET',
       headers:{
-        'Content-Type':'application/json;charset=UTF-8'
+        'Content-Type':'application/xhtml+xml;charset=UTF-8'
       },
       mode:'cors',
       cache:'default'
     }).then(res=>{
-      console.log('fetch res',res);
+      console.log('get html res',res);
     })
 
+    fetch('http://ng56.nisco.cn/transrev/list?moduleListNavId=Transrev&_=1561286375733',{
+      method:'GET',
+      headers:{
+        'Content-Type':'text/html'
+      },
+    }).then(res=>{
+      console.log('get html res2222:',res);
+    })
+
+
+  }
+
+  postTestHandler = ()=>{
+
+    fetch('http://localhost:7002/post',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json;charset=UTF-8'
+      }
+
+    }).then(response => response.json()).then(res => {
+      console.log("post:",res)
+  })
   }
   
 
@@ -30,6 +54,8 @@ class App extends React.Component{
       <div className="App">
         <img src={ displayImg } style={{height:400}}/>
         <div>  TEstTest </div>
+
+        <Button onClick={this.postTestHandler}>  测试 </Button>
       </div>
     );
   }
